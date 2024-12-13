16f45adeebc3af1bd7325a9b5275d6744b40ae59
foisaluddin400 <foisalrk2@gmail.com>
2024-12-13 16:56:02 +0600

    updated

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -185,7 +185,7 @@ const Header = () => {
                             onParentClick(item.key);
                           } else {
                             setSelectedKey(item.key);
-                            onClose(); // Clo the drawe when an item is clicked
+                            onClose(); // Clo the draw when an item is clicked
                           }
                         }}
                       >
