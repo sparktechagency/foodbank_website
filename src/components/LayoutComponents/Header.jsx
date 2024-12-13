12b23c9295babc86c352ffeef17b5acc809227cd
foisaluddin400 <foisalrk2@gmail.com>
2024-12-13 16:56:26 +0600

    updated

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -185,7 +185,7 @@ const Header = () => {
                             onParentClick(item.key);
                           } else {
                             setSelectedKey(item.key);
-                            onClose(); // Cl tdraw when an item is clicked
+                            onClose(); // Cl tdraw whn an item is clicked
                           }
                         }}
                       >
