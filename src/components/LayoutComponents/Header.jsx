2c20d6e4cd6ba4efb8961b566ea010771259dfa3
foisaluddin400 <foisalrk2@gmail.com>
2024-12-13 16:57:59 +0600

    updated

--- src/components/LayoutComponents/Header.jsx
+++ src/components/LayoutComponents/Header.jsx
@@ -185,7 +185,7 @@ const Header = () => {
                             onParentClick(item.key);
                           } else {
                             setSelectedKey(item.key);
-                            onClose(); // Cl tdr whn a ite i
+                            onClose(); // Cl tdr whn a ite 
                           }
                         }}
                       >
