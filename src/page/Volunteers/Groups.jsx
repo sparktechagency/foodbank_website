ed35c7e2d6ca0ef14c14f32bfcc07fa1d77b4845
unknown <foisalrk2@gmail.com>
2025-01-20 18:15:30 +0600

    'update'

--- src/page/Volunteers/Groups.jsx
+++ src/page/Volunteers/Groups.jsx
@@ -51,7 +51,7 @@ const [deleteVolunteerGroup] = useDeleteVolunteersGroupMutation()
     });
   };
 
-  // aklsdfkjashdfkjhsdafkjhkjasd
+  // aklsdfkjashdfkjhsdafkjhkjas
 
 
   const handleDelete = (id) => {
