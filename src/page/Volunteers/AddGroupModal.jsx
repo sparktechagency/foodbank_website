950ac2d7d67ab0ffbfb1917975b5b2d218609f0c
unknown <foisalrk2@gmail.com>
2025-01-21 20:55:45 +0600

    'update'

--- src/page/Volunteers/AddGroupModal.jsx
+++ src/page/Volunteers/AddGroupModal.jsx
@@ -52,7 +52,7 @@ console.log(clientOptions)
       console.error("API Error:", error);
     }
   };
-//asdfjklsadffdsgf
+//asdfjklsadffdsg
   return (
     <Modal
       title="Create New Volunteer Group"
