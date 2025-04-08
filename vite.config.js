447c9b257abeaac21a11635f62c850e62e4ead98
TayeburRahman <tayebrayhan101+@gmail.com>
2025-04-08 19:46:57 +0600

    updates all the codes 7-9.32

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: "172.0.1.108",
+    host: "172.21.112.1",
     port: "3000",
   },
 })
-// 24.199.120.27
+// 172.0.1.108
